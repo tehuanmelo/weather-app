ed42d493623667a01c4f7b0c62bac56fac869c85
tehuanmelo <tehuanmelo@gmail.com>
2025-04-28 19:08:35 +0400

    create the ui and style

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -10,12 +10,33 @@ import search_icon from '../assets/search.png'
 import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 
+const api_key = "d0b5658fd2330eda3003dc681033088e"
+
 const Weather = () => {
   return (
     <div className='weather'>
         <div className="search-bar">
             <input type="text" placeholder='search' />
             <img src={search_icon} alt="" />
+        </div>
+        <img src={clear_icon} alt="" className='weather-icon'/>
+        <p className='temperature'>16°C</p>
+        <p className='location'>London</p>
+        <div className="weather-data">
+            <div className="col">
+                <img src={humidity_icon} alt="" />
+                <div>
+                    <p>91%</p>
+                    <span>Humidity</span>
+                </div>
+            </div>
+            <div className="col">
+                <img src={wind_icon} alt="" />
+                <div>
+                    <p>30km/h</p>
+                    <span>Wind Speed</span>
+                </div>
+            </div>
         </div>
     </div>
   )
